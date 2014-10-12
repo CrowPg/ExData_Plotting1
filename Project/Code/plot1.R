9173f563507d41230cac91ddc60cd4d3c7c2af8d
Johan Medioni <crow13pg@aol.fr>
2014-10-13 01:09:25 +0200

    1st commit

---
+++ Project/Code/plot1.R
@@ -0,0 +1,5 @@
+PowerCons <- read.table("household_power_consumption.txt", header = T, stringsAsFactors = F, sep = ";", skip = 66636, nrows = 2880, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity","Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = "?")
+
+png("plot1.png", width = 480, height = 480)
+hist(PowerCons$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylim = c(0, 1200))
+dev.off()
