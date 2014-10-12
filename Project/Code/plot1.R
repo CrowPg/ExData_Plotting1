3919726cddebd7914952ba5b9673d99d7cf3ef2f
Johan Medioni <crow13pg@aol.fr>
2014-10-13 01:23:10 +0200

    2nd commit

--- Project/Code/plot1.R
+++ Project/Code/plot1.R
@@ -1,4 +1,10 @@
+#PowerCons <- read.table("household_power_consumption.txt", header = T, stringsAsFactors = F, sep = ";", skip = 66636, nrows = 2880, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity","Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = "?")
+#PowerCons$Date <- as.Date(PowerCons$Date, format = "%d/%m/%Y")
+#strptime(PowerCons$Time, format = "%H:%M:%S")
+
 PowerCons <- read.table("household_power_consumption.txt", header = T, stringsAsFactors = F, sep = ";", skip = 66636, nrows = 2880, col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity","Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = "?")
+PowerCons$Date <- paste(PowerCons$Date, PowerCons$Time, sep = " ")
+PowerCons$Date <- strptime(PowerCons$Date, format="%d/%m/%Y %H:%M:%S")
 
 png("plot1.png", width = 480, height = 480)
 hist(PowerCons$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylim = c(0, 1200))
---
+++ Project/Code/plot2.R
@@ -0,0 +1,1 @@
+with(PowerCons, plot(Date, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)"))
